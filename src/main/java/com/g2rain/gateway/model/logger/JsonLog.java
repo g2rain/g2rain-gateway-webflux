fa9fbf2458d523941bfe62ef751679e1c1f2d1ee
xingbaosun <sunxingbao7@126.com>
2026-04-19 18:27:50 +0800

    webflux 版本网关

--- src/main/java/com/g2rain/gateway/model/logger/JsonLog.java
+++ src/main/java/com/g2rain/gateway/model/logger/JsonLog.java
@@ -13,15 +13,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class JsonLog {
-    // DPoP Authorization 不需要 Header不需要， 已将需要上送的Header拆分到具体的EdgePrincipalContext字段！！！！
-
-    // kafka能否做成异步推送，不影响请求的响应时间!!!!
-    // EdgePrincipalContext
-    // Request queryString, body {"a":[1,2]} queryString 和 body 合并
-    // user-Agent, Host x-forward-ip -x-real-ip referer
-    // Response (Json)
-    // traceId，requestId
-    // {"payload":{"request":{}, "response":{}}}
     private String level;
     private String traceId;
     private String requestId;
